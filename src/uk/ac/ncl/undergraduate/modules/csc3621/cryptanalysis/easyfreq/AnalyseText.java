2cad47a790a0b139f5f902890c021933e682158d
Yath <yatharthasharma09@gmail.com>
2018-10-24 12:16:59 +0100

    changes

--- src/uk/ac/ncl/undergraduate/modules/csc3621/cryptanalysis/easyfreq/AnalyseText.java
+++ src/uk/ac/ncl/undergraduate/modules/csc3621/cryptanalysis/easyfreq/AnalyseText.java
@@ -26,22 +26,40 @@ public class AnalyseText {
 	public static boolean outOfLimits(char x){	
 		return (x > 25 || x < 0);
 	}
-	public static double indexOfCoincidence(String cipherText, int key){
+	public static double indexOfCoincidence(String cipherText, int keyLength){
 		cipherText = cipherText.toUpperCase();
-		String[] arrayOfArrays = new String[cipherText.length()/key];
-		for (int j = 0; j < arrayOfArrays.length; j++){
-			for (int i = 0; i <= cipherText.length(); i++){
-				arrayOfArrays[j] += cipherText.charAt(i);
-				i += key;
+		String[] arrayOfSubstrings = new String[keyLength];
+		int total = 0;
+		for (int j = 0; j < arrayOfSubstrings.length; j++){
+			for (int i = j; i <= cipherText.length(); i++){
+				arrayOfSubstrings[j] += cipherText.charAt(i);
+				i += keyLength;
 			}
+		}
+		for (int j = 0; j < arrayOfSubstrings.length; j++){
+			total += averageIndexOfCoincidence(arrayOfSubstrings);
 		}
 		return 0d;
 	}
 	public static double averageIndexOfCoincidence(String[] x){
-		
-		return 0d;
+		double total = 0d;
+		for (int i = 0; i < x.length; i++){
+			total += result(x[i]);
+		}
+		return total/x.length;
 	}
-	public static int[] numberOfLetters (String cipherText, char x){
+	public static double result(String x){
+		double xyz = 0d;
+		for (char i = 'A'; i <= 'Z'; i++){
+			xyz += individualIC(numberOfLetters(x, i));
+		}
+		return xyz;
+	}
+	public static double individualIC(int[] x){					// for one letter in a broken ciphertext
+		double z = x[0];
+		return (z * (z - 1)) / (x[1] * (x[1] - 1));
+	}
+	public static int[] numberOfLetters (String cipherText, char x){		// number of a specific letter in the given CT and the total count of letters
 		Map<Character, Double> freq = new HashMap<Character, Double>();
 		int totalCountOfLetters = 0;
 		for (char xyz = 'A'; xyz <= 'Z'; xyz++) {
